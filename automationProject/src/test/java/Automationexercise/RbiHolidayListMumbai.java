ab67e86bc0bf5787c8228bb42bc34dc6323e6d65
Jayshree-90 <nitamitali60@gmail.com>
2025-09-04 16:41:01 +0530

    Table into text file

--- automationProject/src/test/java/Automationexercise/RbiHolidayListMumbai.java
+++ automationProject/src/test/java/Automationexercise/RbiHolidayListMumbai.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -32,48 +33,19 @@ public class RbiHolidayListMumbai {
 		// Locate the holiday table
 		WebElement holidayTable = driver.findElement(By.xpath("(//table[@role='presentation'])[2]/tbody"));
 
-		// Get all rows
-		List<WebElement> rows = holidayTable.findElements(By.tagName("tr"));
-
-		// Prepare file writer
+		List<WebElement> li = holidayTable.findElements(By.xpath("(//table[@role='presentation'])[2]/tbody/tr"));
+        
 		BufferedWriter writer = new BufferedWriter(new FileWriter("./RBI_Holiday_List.txt"));
-		System.out.println("Extracting RBI Holidays...");
-		writer.write("RBI Holidays (Mumbai, All Months)\n\n");
-
-		for (WebElement row : rows) {
-			// Check if row contains header cells (th)
-			List<WebElement> headerCells = row.findElements(By.tagName("th"));
-
-			if (!headerCells.isEmpty()) {
-				String headerText = headerCells.get(0).getText().trim();
-				System.out.println("\n " + headerText);
-				writer.write("\n" + headerText + " \n");
-				List<WebElement> cols = driver.findElements(By.tagName("td"));
-				int colnum = cols.size();
-				System.out.println(colnum);
-				for (int mon = 1; mon <= headerCells.size() - 1; mon++) {
-					if (!headerText.isEmpty()) {
-						for (int i = 6; i <= colnum - 1; i++) {
-							String columns = row.findElement(By.xpath("(//td)[" + i + "]")).getText();
-							System.out.println(columns + " , " + mon);
-						}
-					}
-				}
-
-				/*
-				 * if (columns.size() >= 3) { String dateFull = cols.get(0).getText().trim(); //
-				 * e.g., "26 February 2025" String occasion = cols.get(1).getText().trim();
-				 * //String occasion1 = cols.get(2).getText().trim();
-				 * 
-				 * 
-				 * String formatted = headerText+" , "+dateFull + " , " + occasion;
-				 * System.out.println(formatted); writer.write(formatted + "\n");
-				 * 
-				 * } }
-				 */ }
+		 System.out.println("Extracting RBI Holidays...");
+		 writer.write("RBI Holidays (Mumbai, All Months)\n\n");
+		System.out.println("============fest=================");
+		for (WebElement mr :li) {
+		System.out.println(mr.getText()+"\n");
+		writer.write(mr.getText()+"\n");
 		}
-
 		writer.close();
-		driver.quit();
+		
+				 
+		  driver.quit();
 	}
 }
